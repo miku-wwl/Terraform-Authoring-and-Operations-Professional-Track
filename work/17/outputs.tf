99e9f557e1fb8b64bf5827df09d483b1d3db03af
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-07-04 00:17:15 +1200

    lab17: complete plan scan workflow exercise

--- work/17/outputs.tf
+++ work/17/outputs.tf
@@ -1,12 +1,8 @@
-# TODO 1：补充把二进制 plan 转成 JSON 的命令。
-# TODO 2：补充针对 plan.json 的 Checkov plan scan 命令。
-# 提示：terraform show -json tfplan > plan.json 转换格式；
-# checkov -f plan.json --framework terraform_plan --check CKV_K8S_19 --soft-fail 扫描 plan。
 output "plan_scan_commands" {
   description = "plan level scan 的核心命令。"
   value = [
     "terraform plan -input=false -no-color -out=tfplan -var host_network_enabled=true",
-    "TODO：补充把二进制 plan 转成 JSON 的命令",
-    "TODO：补充针对 plan.json 的 Checkov plan scan 命令",
+    "terraform show -json tfplan > plan.json",
+    "checkov -f plan.json --framework terraform_plan --check CKV_K8S_19 --soft-fail",
   ]
 }
