cb18ce33264f843ee0113c2ad9fc02a774a34a06
miku-wwl <569144003@qq.com>
2026-07-08 01:23:21 +1200

    Complete lab 92 module improvements

--- work/92/modules/ec2_instance/main.tf
+++ work/92/modules/ec2_instance/main.tf
@@ -1,13 +1,7 @@
 locals {
-  # TODO 1: Replace this hard-coded AMI with var.ami_id.
-  selected_ami_id = "ami-0123456789abcdef0"
+  selected_ami_id = var.ami_id
 
-  # TODO 2: Replace this hard-coded instance type with var.instance_type.
-  selected_instance_type = "t2.micro"
-
-  # TODO 3: Remove module-owned region from this module.
-  # The caller's provider configuration should decide where AWS resources run.
-  selected_region = "us-east-1"
+  selected_instance_type = var.instance_type
 }
 
 resource "terraform_data" "ec2_plan" {
@@ -15,7 +9,6 @@ resource "terraform_data" "ec2_plan" {
     ami_id        = local.selected_ami_id
     instance_type = local.selected_instance_type
     instance_name = var.instance_name
-    region        = local.selected_region
   }
 }
 
--- work/92/modules/ec2_instance/versions.tf
+++ work/92/modules/ec2_instance/versions.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 1.5.0"
 
-  # TODO 4: Add the AWS required_providers block here.
-  # Hint: source should be hashicorp/aws, version should be >= 5.5.
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.5"
+    }
+  }
 }
