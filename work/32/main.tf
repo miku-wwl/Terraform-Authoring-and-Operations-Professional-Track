f460ba9b85a68325fa380053a3e135cdcf705652
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-07-08 15:23:03 +1200

    Improve lab 32 AWS LocalStack starter

--- work/32/main.tf
+++ work/32/main.tf
@@ -1,3 +1,5 @@
-# TODO：声明 data "aws_caller_identity"。
+# TODO 1: Read the current AWS caller identity from the configured provider.
+# Hint: use data "aws_caller_identity" "current" {}.
 
-# TODO：声明 data "aws_region"。
+# TODO 2: Read the current AWS provider region.
+# Hint: use data "aws_region" "current" {}.
--- work/32/outputs.tf
+++ work/32/outputs.tf
@@ -1,5 +1,11 @@
-# TODO：输出 account_id。
+# TODO 3: Output the current account ID.
+# Output name must be account_id.
+# Hint: value should be data.aws_caller_identity.current.account_id.
 
-# TODO：输出 caller_arn。
+# TODO 4: Output the current caller ARN.
+# Output name must be caller_arn.
+# Hint: value should be data.aws_caller_identity.current.arn.
 
-# TODO：输出 current_region。
+# TODO 5: Output the current provider region.
+# Output name must be current_region.
+# Hint: value should be data.aws_region.current.name.
