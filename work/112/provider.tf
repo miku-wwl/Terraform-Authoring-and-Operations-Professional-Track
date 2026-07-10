b30c56efaf5fa4c2640725c48707bceb3bb71452
Weilai Wang <38804125+miku-wwl@users.noreply.github.com>
2026-07-10 13:53:18 +1200

    Complete lab 112 assume role exercise

--- work/112/provider.tf
+++ work/112/provider.tf
@@ -19,12 +19,9 @@ provider "aws" {
     iam = var.localstack_endpoint
   }
 
-  # TODO: 在 provider 中配置 assume_role。
-  # Hint：把下面这段取消注释即可。
-  #
-  # assume_role {
-  #   # 真实 AWS 中这里通常是目标账号/目标角色 ARN；本实验使用 LocalStack 模拟 ARN。
-  #   role_arn     = "arn:aws:iam::000000000000:role/tf-pro-lab-112"
-  #   session_name = "tf-pro-lab-112"
-  # }
+  assume_role {
+    # 真实 AWS 中这里通常是目标账号/目标角色 ARN；本实验使用 LocalStack 模拟 ARN。
+    role_arn     = "arn:aws:iam::000000000000:role/tf-pro-lab-112"
+    session_name = "tf-pro-lab-112"
+  }
 }
